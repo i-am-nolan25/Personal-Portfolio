15e4e083ca460a4351ddfb5822655fd004178c36
i-am-nolan25 <nl020@bucknell.edu>
2023-07-20 06:37:42 -0400

    Add Instagram Basic Display API

--- javascript/script.js
+++ javascript/script.js
@@ -71,7 +71,7 @@ for (let i = 0; i < filterBtn.length; i++) {
 
 }
 
-
+// Note: The purpose of this JavaScript code, linked with the PHP code, is to get message/data sent by someone using my "Contact" form on my website. However, as Github doesn't support to send a message using a back-end server on their github.io website, I couldn't really utilize it. My current solution is adding the third-party service called Formspree.io (free version) and trying to achieve my goal. 
 // contact form variables
 
 // // Get the form element
@@ -154,3 +154,77 @@ audioIcon.addEventListener("click", () => {
     audioElement.pause();
   }
 });
+
+
+// Instagram Basic Display API
+
+//Generate access token using meta for developers
+const accessToken = 'IGQVJYaXlNVno0X0o1OGpnclpOU0o1aDA4enFLR1RWV1p1bXdqX01qcUdMM3BRYVdZAejk3ZAnR3eGFyUWVmek51alk2SHNkWklHNllQaGhXWC1qYTRoaTI4NmxkVWs3TjB1ZAXJkdXFpbVpDZAm9hblNSNAZDZD';
+
+//Generate user id using "https://graph.instagram.com/me?fields=id,username&access_token="
+const userId = '6368860399876693';
+
+const instagramFeed = document.getElementById('instagram-feed');
+
+// Function to fetch media for a given URL
+async function fetchMedia(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+// Fetch a specific number of media items from the user's Instagram feed using custom batching
+async function fetchInstagramMediaWithLimit(desiredLimit) {
+  const batchLimit = 50; // Number of media items to fetch in each batch
+  let allMedia = [];
+  let nextPageURL = null;
+
+  while (allMedia.length < desiredLimit) {
+    const batchURL = nextPageURL
+      ? nextPageURL
+      : `https://graph.instagram.com/6368860399876693/media?fields=id,media_type,media_url,permalink,timestamp,caption&access_token=IGQVJYaXlNVno0X0o1OGpnclpOU0o1aDA4enFLR1RWV1p1bXdqX01qcUdMM3BRYVdZAejk3ZAnR3eGFyUWVmek51alk2SHNkWklHNllQaGhXWC1qYTRoaTI4NmxkVWs3TjB1ZAXJkdXFpbVpDZAm9hblNSNAZDZD&limit=100`;
+
+    const { data, paging } = await fetchMedia(batchURL);
+
+    // Filter media items based on the presence of a specific hashtag in the caption text
+    const filteredMedia = data.filter(item => item.caption && item.caption.includes('#nolan'));
+
+    allMedia.push(...filteredMedia);
+    nextPageURL = paging?.next;
+
+    if (!nextPageURL) break; // Exit the loop if there are no more pages
+  }
+
+  // Sort media items by timestamp in descending order (most recent first)
+  allMedia.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+
+  return allMedia.slice(0, desiredLimit); // Return only the desired number of media items
+}
+
+// Process the retrieved data and generate HTML for the feed
+function generateFeedHTML(media) {
+  let html = '<div class="grid">';
+  media.forEach(post => {
+    if (post.media_type === 'IMAGE') {
+      html += `
+        <a href="${post.permalink}" target="_blank" rel="noopener">
+          <img src="${post.media_url}" alt="Instagram Photo">
+        </a>
+      `;
+    }
+  });
+  html += '</div>';
+  return html;
+}
+
+// Fetch the Instagram feed and display it with the desired limit
+const desiredPhotoLimit = 12; // Change this value to the desired number of photos
+fetchInstagramMediaWithLimit(desiredPhotoLimit)
+  .then(media => {
+    const html = generateFeedHTML(media);
+    instagramFeed.innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Error fetching Instagram feed:', error);
+    instagramFeed.innerHTML = 'Failed to fetch Instagram feed.';
+  });
